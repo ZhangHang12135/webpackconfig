47d75aba3b54d48eebe220d13c4bd32acaf46454
zhanghang12135 <z839187405@qq.com>
2023-02-05 15:25:26 +0800

    管理资源

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,9 +2,21 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './script/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: 'bundle.js',
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
 }
